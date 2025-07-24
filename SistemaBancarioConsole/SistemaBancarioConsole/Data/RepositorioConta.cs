d40db65de0f28e55f499e44cf661e52bfe3e129f
Tercio Gabriel <terciogabrieldejesus@gmail.com>
2025-07-24 20:11:41 -0300

    att repositorios

--- SistemaBancarioConsole/SistemaBancarioConsole/Data/RepositorioConta.cs
+++ SistemaBancarioConsole/SistemaBancarioConsole/Data/RepositorioConta.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaBancarioConsole.Models;
+using SistemaBancarioConsole.Utils;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,73 @@ namespace SistemaBancarioConsole.Data
 {
     internal class RepositorioConta
     {
+        private List<ContaBancaria> _contas;
+        private const string NOME_ARQUIVO_CONTAS = "contas.txt";
+
+        public RepositorioConta()
+        {
+            _contas = new List<ContaBancaria>();
+            //CarregarContas();
+        }
+
+        private void CarregarContas()
+        {
+            var linhasDoArquivo = ArquivoHelper.LerLinhas(NOME_ARQUIVO_CONTAS);
+            _contas.Clear();
+            foreach (var linha in linhasDoArquivo)
+            {
+                var partes = linha.Split(',');
+                string saldoString = partes[1];
+                decimal saldo;
+
+                if (decimal.TryParse(saldoString, NumberStyles.Any, CultureInfo.InvariantCulture, out saldo))
+                {
+                    string numeroConta = partes[0];
+                    string nomeUsuario = partes[2];
+                    if (partes.Length == 3)
+                    {
+                        ContaBancaria conta = new ContaBancaria(numeroConta, saldo, nomeUsuario);
+                        _contas.Add(conta);
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine($"Erro ao carregar conta: Saldo inválido '{saldoString}' na linha '{linha}'.");
+                }
+            }
+        }
+        public void SalvarContas()
+        {
+            List<string> linhasParaEscrever = new List<string>();
+
+            foreach (var conta in _contas)
+            {
+                string linhaDoArquivoParaSalvar = $"{conta.NomeUsuario},{conta.Saldo}, {conta.NumeroConta}";
+                linhasParaEscrever.Add(linhaDoArquivoParaSalvar);
+            }
+
+            ArquivoHelper.EscreverLinhas(NOME_ARQUIVO_CONTAS, linhasParaEscrever);
+        }
+
+        public void AdicionarConta(ContaBancaria novaConta)
+        {
+            _contas.Add(novaConta);
+            SalvarContas();
+        }
+
+        public ContaBancaria BuscarPorNumeroConta(string numConta)
+        {
+            foreach (var conta in _contas)
+            {
+                if (conta.NumeroConta.Equals(numConta, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Conta encontrada: {conta.NumeroConta}");
+                    return conta;
+                }
+            }
+
+            return null;
+        }
     }
 }
--- SistemaBancarioConsole/SistemaBancarioConsole/Models/ContaBancaria.cs
+++ SistemaBancarioConsole/SistemaBancarioConsole/Models/ContaBancaria.cs
@@ -8,5 +8,15 @@ namespace SistemaBancarioConsole.Models
 {
     internal class ContaBancaria
     {
+        public string NumeroConta { get; set; }
+        public decimal Saldo { get; set; }
+        public string NomeUsuario { get; set; }
+        
+        public ContaBancaria(string numeroConta, decimal saldo, string nomeUsuario)
+        {
+            NumeroConta = numeroConta;
+            Saldo = 0m;
+            NomeUsuario = nomeUsuario;
+        }
     }
 }
